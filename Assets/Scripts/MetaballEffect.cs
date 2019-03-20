1f1d6e711bf59230f0808a64978d50f8a5f711af
SakumaTakuya <mo-takusan9922@docomo.ne.jp>
2019-03-20 16:09:10 +0900

    fist

---
+++ Assets/Scripts/MetaballEffect.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Sakkun.Metaball
+{
+    [ExecuteInEditMode]
+    public class MetaballEffect : MonoBehaviour
+    {
+        [SerializeField] private float _cut;
+        [SerializeField] private Shader _shader;
+        private Material _material;
+        private int _cutID;
+
+        private void Awake()
+        {
+            _material = new Material(_shader);
+            _cutID = Shader.PropertyToID("_Cut");
+        }
+
+        private void OnRenderImage(RenderTexture src, RenderTexture dest)
+        {
+            _material.SetFloat(_cutID, _cut);
+            Graphics.Blit(src, dest, _material);
+        }
+    }
+}
